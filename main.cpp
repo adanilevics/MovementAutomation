0513f377df7e06f24828061666c6c6ba0e6801e2
adanilevics <110359798+adanilevics@users.noreply.github.com>
2024-03-15 00:16:00 +0200

    Added client coordinate based movement

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include "mousemove.h"
 #include <Windows.h>
+#include <iostream>
+
 
 int main() {
+
+
+    Wait(1000);
+
     int startX = 100;
     int startY = 100;
     int endX = 500;
@@ -30,6 +36,16 @@ int main() {
 
     Click(); // click once
     Click(100); // 100ms wait between pressing and releasing the mouse button
+
+    Wait(1000);
+
+    int windowX = 560; 
+    int windowY = 480;
+
+    // move to the windowX and windowY positions in the active window using client coordinates
+    moveMouseRandomCurvedClient(windowX, windowY, 600);
+
+    Wait(5000);
 
     return 0;
 }
--- mousemove.h
+++ mousemove.h
@@ -6,5 +6,6 @@
 void moveMouseRandomCurved(int startX, int startY, int endX, int endY, int durationMs);
 void Wait(int milliseconds);
 void Click(int delayBetweenClicksMs = 0);
+void moveMouseRandomCurvedClient(int windowX, int windowY, int durationMs);
 
 #endif // MOUSEMOVE_H
