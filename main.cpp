3fb92e9c54da194d4c75734fe6877201807f2735
adanilevics <110359798+adanilevics@users.noreply.github.com>
2024-03-14 21:25:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,27 +2,30 @@
 #include <Windows.h>
 
 int main() {
+
+    // defining coordinates
     int startX = 100;
     int startY = 100;
     int endX = 500;
     int endY = 500;
 
-    // Define the duration in milliseconds
     int durationMs = 600; // 0.6 seconds
-
-    // Call moveMouseRandomCurved function
     moveMouseRandomCurved(startX, startY, endX, endY, durationMs);
 
-    // Get the current mouse position
+    // get the current mouse position
     POINT currentPosition1;
     GetCursorPos(&currentPosition1);
 
-    // Move to three other places based on the current mouse position
+    // move to three other places based on the current mouse position
     moveMouseRandomCurved(currentPosition1.x, currentPosition1.y, 300, 200, durationMs);
-    GetCursorPos(&currentPosition1); // Recapture the current mouse position
+    GetCursorPos(&currentPosition1); // recapture the current mouse position
     moveMouseRandomCurved(currentPosition1.x, currentPosition1.y, 700, 400, durationMs);
-    GetCursorPos(&currentPosition1); // Recapture the current mouse position
+    GetCursorPos(&currentPosition1); // recapture the current mouse position
     moveMouseRandomCurved(currentPosition1.x, currentPosition1.y, 200, 600, durationMs);
+    
+    GetCursorPos(&currentPosition1); 
+    moveMouseRandomCurved(currentPosition1.x, currentPosition1.y, 300, 200, 700); // preform movement from current mouse position to 300,200 with a duration of 0.7 ms
+
 
     return 0;
 }
